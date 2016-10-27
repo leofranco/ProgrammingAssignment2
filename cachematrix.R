cb9b6693ca8b9d9db37f604fe4bf09409e3b89b3
Leo Franco <leo.franco@gmail.com>
2016-10-27 16:57:29 +0800

    Adding comments to code

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## LF 2016-10-27
+## The idea of this assigment is to learn how
+## to cache potentially expensive operations
+## An example is given about how to cache
+## the mean of a vector and I use the same
+## code but changing the "mean" function
+## by the "solve" function which will
+## invert a matrix instead.
+## Please not that the determinant cannot
+## be zero for us to be able to invert a
+## matrix
 
-## Write a short comment describing this function
-
+## This function creates a special 
+## matrix object with the cache
+## the idea is that we will only
+## compute the inverse once.
+## If it has already been computed
+## then we only return the value
+## Note that when the matrix is set
+## we have to empty the cache so that
+## it is recalculated again.
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
@@ -17,9 +33,11 @@ makeCacheMatrix <- function(x = matrix()) {
              getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
+## This function behaves as a simple cache
+## if the function has already been called
+## then we return the cached data
+## otherwise we will use the solve function
+## to invert the matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         m <- x$getsolve()
